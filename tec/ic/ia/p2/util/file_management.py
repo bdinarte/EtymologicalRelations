af3d92bb36d22aa8b473d47dbd198b871e502e18
Brandon <bdinarte@yahoo.com>
2018-06-10 21:14:31 -0600

    get file content in file_management

--- tec/ic/ia/p2/util/file_management.py
+++ tec/ic/ia/p2/util/file_management.py
@@ -7,9 +7,19 @@ from os import path as ospath
 # -----------------------------------------------------------------------------
 
 
-def get_etim_database(base_path, filename="etymwn.tsv"):
+def get_etim_database(base_path, filename='etymwn.tsv'):
 
-    database_path = ospath.join(base_path, "files", filename)
+    database_path = ospath.join(base_path, 'files', filename)
     data = read_csv(database_path, sep='\t', header=None)
 
     return data
+
+
+def get_file_content(base_path, filename):
+
+    file_path = ospath.join(base_path, 'files', filename)
+
+    with open(file_path, encoding='utf-8') as file_content:
+        content = file_content.read()
+
+    return content
