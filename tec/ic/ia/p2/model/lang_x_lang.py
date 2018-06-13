39d8b905c9611a9948a7465037790091a594c1a8
Brandon <bdinarte@yahoo.com>
2018-06-12 18:26:54 -0600

    added all relation types

--- tec/ic/ia/p2/model/lang_x_lang.py
+++ tec/ic/ia/p2/model/lang_x_lang.py
@@ -7,24 +7,57 @@ from sys import path as syspath
 from ..util.file_management import get_etim_database
 from ..model.data_management import assert_facts_from_dataframe
 
+
 # ----------------------------------------------------------------------------
 
 # Definición de términos comunes a la mayoría de predicados
-pyDatalog.create_terms('etymological_origin_of, has_derived_form,'
-                       'is_derived_from, etymology, etymologically_related,'
-                       'orthography')
+pyDatalog.create_terms('etymological_origin_of_active,'
+                       'has_derived_form_active,'
+                       'is_derived_from_active,'
+                       'etymology_active,'
+                       'etymologically_related_active')
+pyDatalog.create_terms('etymological_origin_of,'
+                       'has_derived_form,'
+                       'is_derived_from,'
+                       'etymology,'
+                       'etymologically_related')
 pyDatalog.create_terms('Lang1, Lang2, Word1, Word2')
 
 # ----------------------------------------------------------------------------
+
++ etymological_origin_of('','','','')
++ has_derived_form('','','','')
++ is_derived_from('','','','')
++ etymology('','','','')
++ etymologically_related('','','','')
++ etymological_origin_of_active(True)
++ has_derived_form_active(True)
++ is_derived_from_active(True)
++ etymology_active(True)
++ etymologically_related_active(True)
 
 # Términos para la función words_in_common
 pyDatalog.create_terms('lang_common_words')
 # Relaciones para la función words_in_common
 lang_common_words(Lang1, Word1, Lang2, Word1) <= (
-    etymological_origin_of(Lang1, Word1, Lang2, Word1)
+    etymological_origin_of(Lang1, Word1, Lang2, Word1) &
+    etymological_origin_of_active(True)
 )
 lang_common_words(Lang1, Word1, Lang2, Word1) <= (
-    etymology(Lang1, Word1, Lang2, Word1)
+    has_derived_form(Lang1, Word1, Lang2, Word1) &
+    has_derived_form_active(True)
+)
+lang_common_words(Lang1, Word1, Lang2, Word1) <= (
+    is_derived_from(Lang1, Word1, Lang2, Word1) &
+    is_derived_from_active(True)
+)
+lang_common_words(Lang1, Word1, Lang2, Word1) <= (
+    etymology(Lang1, Word1, Lang2, Word1) &
+    etymology_active(True)
+)
+lang_common_words(Lang1, Word1, Lang2, Word1) <= (
+    etymologically_related(Lang1, Word1, Lang2, Word1) &
+    etymologically_related_active(True)
 )
 
 
@@ -38,7 +71,7 @@ def words_in_common(language1, language2):
     if not query_results.v():
         return ['No hay palabras en común.']
 
-    existing_words = [word_tuple[0] for word_tuple in list(query_results)]
+    existing_words = [word_tuple[0] for word_tuple in query_results.data]
     return existing_words
 
 
