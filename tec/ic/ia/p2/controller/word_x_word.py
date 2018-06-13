de23fa16f24b2dc9fa624931fbe09690792bba5e
Julian Salinas <julian4001@outlook.com>
2018-06-13 02:49:24 -0600

    controller word_x_word

--- tec/ic/ia/p2/controller/word_x_word.py
+++ tec/ic/ia/p2/controller/word_x_word.py
@@ -5,29 +5,32 @@ from model.word_x_word import *
 # -----------------------------------------------------------------------------
 
 def exec_are_siblings(first_word, second_word):
-    return are_siblings(first_word, second_word, R).v()[0]
+    are_siblings = are_siblings(first_word, second_word, R).v()[0]
+    return "SI" if are_siblings else "NO"
 
 # -----------------------------------------------------------------------------
 
 def exec_are_cousins(first_word, second_word):
-    query = are_cousins(first_word, second_word, R)
-
-    return are_cousins(first_word, second_word, R).v()[0]
+    are_cousins = are_cousins(first_word, second_word, R).v()[0]
+    return "SI" if are_cousins else "NO"
 
 # -----------------------------------------------------------------------------
 
 def exec_is_child(parent_word, child_word):
-    return is_child(parent_word, child_word, R).v()[0]
+    is_child = is_child(parent_word, child_word, R).v()[0]
+    return "SI" if is_child else "NO"
 
 # -----------------------------------------------------------------------------
 
 def exec_is_uncle(uncle_word, nephew_word):
-    return is_uncle(uncle_word, nephew_word, R).v()[0]
+    is_uncle = is_uncle(uncle_word, nephew_word, R).v()[0]
+    return "SI" if is_child else "NO"
 
 # -----------------------------------------------------------------------------
 
 def exec_cousins_level(first_word, second_word):
-    return cousins_level(first_word, second_word, L).v()[0]
+    level = cousins_level(first_word, second_word, L).v()[0]
+    return "No son primos" if level is 0 else "Son primos " + str(level) + "°"
 
 # -----------------------------------------------------------------------------
 
