36e22bd2394aace585a23226483c684bb4cf177b
Brandon <bdinarte@yahoo.com>
2018-06-15 19:54:21 -0600

    langxlang tests everywhere

--- tec/ic/ia/p2/tests/test_lang_x_lang.py
+++ tec/ic/ia/p2/tests/test_lang_x_lang.py
@@ -9,69 +9,97 @@ from ..controller.lang_x_lang import *
 
 
 def setup_module(module):
-    + etymology('abs', 'beta', 'zsm', 'beta')
-    + etymology('aaq', 'prueba1', 'eng', 'prueba1')
-    + etymology('aaq', 'senabe', 'eng', 'sannup')
-    + etymological_origin_of('abe', 'waniigan', 'eng', 'waniigan')
-    + etymological_origin_of('aaq', 'prueba2', 'eng', 'prueba2')
-    + etymology('aaq', 'prueba3', 'eng', 'prueba3')
-    + has_derived_form('equ', 'father', 'isd', 'son')
-    + has_derived_form('equ', 'father', 'isd', 'son2')
-    + etymology('isd', 'son3', 'equ', 'father3')
-    + etymological_origin_of('sas', 'mom', 'isd', 'son4')
-    + has_derived_form('sss', 'mom', 'isd', 'son5')
-    + etymologically_related('lla','ma','alp','aca')
+    + etymology("por", "ego", "chi", "padre")
+    + etymology("por", "tio_ter", "chi", "tio_abuelo_seg")
+    + etymology("por", "padre", "chi", "abuelo")
+    + etymology("spa", "tio_seg", "chi", "tio_abuelo")
+
+    + has_derived_form("ind", "tatarabuelo", "por", "bisabuelo")
+    + has_derived_form("ind", "tatarabuelo", "ind", "tio_bisabuelo")
+    + has_derived_form("por", "tio_ter", "lat", "primo_ter")
+    + has_derived_form("por", "bisabuelo", "ape", "abuelo")
+    + has_derived_form("chi", "abuelo", "nor", "tio")
+    + has_derived_form("lat", "padre", "ape", "hermano")
+    + has_derived_form("lat", "tio_seg", "ita", "primo_seg")
+
+    + etymological_origin_of("por", "padre", "spa", "prueba")
+    + etymological_origin_of("ind", "tio_bisabuelo", "ape", "tio_abuelo_seg")
+    + etymological_origin_of("spa", "padre", "spa", "ego")
+    + etymological_origin_of("spa", "bisabuelo", "por", "tio_abuelo")
+    + etymological_origin_of("nor", "tio", "ita", "primo")
+
+    + etymologically_related("spa", "ego", "ape", "hermano")
+    + etymologically_related("nor", "tio", "lat", "padre")
 
 # ----------------------------------------------------------------------------
 
 
-def test_words_in_common():
+def test_words_in_common_exist():
 
     """
     Prueba de la función para obtener las palabras en común entre dos
-    lenguajes
+    lenguajes que si tienen palabras en común
     Entradas: No aplica
     @return Sin retorno
     """
 
-    obtained_results = words_in_common('aaq', 'eng')
-    expected_results = {'prueba1', 'prueba2', 'prueba3'}
-    first_result_success = obtained_results == expected_results
+    obtained_results = words_in_common('spa', 'lat')
+    expected_results = {'padre', 'tio_seg'}
 
-    obtained_results = words_in_common('aaq', 'zsm')
+    assert obtained_results == expected_results
+
+
+def test_words_in_common_dont_exist():
+
+    """
+    Prueba de la función para obtener las palabras en común entre dos
+    lenguajes que no tienen palabras en común
+    Entradas: No aplica
+    @return Sin retorno
+    """
+
+    obtained_results = words_in_common('spa', 'ind')
     expected_results = {'No hay palabras en común.'}
-    second_result_success = obtained_results == expected_results
 
-    assert first_result_success and second_result_success
-
+    assert obtained_results == expected_results
 
 # ----------------------------------------------------------------------------
 
 
-def test_count_common_words():
+def test_count_common_words_count_2():
 
     """
     Prueba de la función para obtener la cantidad de palabras en común
-    entre dos lenguajes
+    entre dos lenguajes que si tienen en común
     Entradas: No aplica
     @return Sin retorno
     """
 
-    obtained_count = count_common_words('aaq', 'eng')
-    expected_count = 3
-    first_count_success = obtained_count == expected_count
+    obtained_count = count_common_words('lat', 'spa')
+    expected_count = 2
 
-    obtained_count = count_common_words('aaq', 'zsm')
+    assert obtained_count == expected_count
+
+
+def test_count_common_words_count_0():
+
+    """
+    Prueba de la función para obtener la cantidad de palabras en común
+    entre dos lenguajes que no tienen en común
+    Entradas: No aplica
+    @return Sin retorno
+    """
+
+    obtained_count = count_common_words('spa', 'nor')
     expected_count = 0
-    second_count_success = obtained_count == expected_count
 
-    assert first_count_success and second_count_success
+    assert obtained_count == expected_count
 
 
 # ----------------------------------------------------------------------------
 
 
-def test_aux_input_words():
+def test_aux_input_words_existing():
 
     """
     Prueba de la función para obtener las palabras que el primer lenguaje
@@ -80,21 +108,30 @@ def test_aux_input_words():
     @return Sin retorno
     """
 
-    obtained_results = aux_input_words('equ', 'isd')
-    expected_results = {'son', 'son2', 'son3'}
-    first_result_success = obtained_results == expected_results
+    obtained_results = aux_input_words('chi', 'por')
+    expected_results = {'ego', 'tio_ter', 'padre'}
 
-    obtained_results = aux_input_words('abs', 'aaq')
+    assert obtained_results == expected_results
+
+
+def test_aux_input_words_not_existing():
+    """
+        Prueba de la función para obtener las palabras que el primer lenguaje
+        aportó al otro cuando no hay aporte
+        Entradas: No aplica
+        @return Sin retorno
+        """
+
+    obtained_results = aux_input_words('nor', 'spa')
     expected_results = {'El lenguaje no aportó nada.'}
-    second_result_success = obtained_results == expected_results
 
-    assert first_result_success and second_result_success
+    assert obtained_results == expected_results
 
 
 # ----------------------------------------------------------------------------
 
 
-def test_aux_count_input_words():
+def test_aux_count_input_words_count_3():
 
     """
     Prueba de la función para obtener la cantidad de palabras que el primer
@@ -103,18 +140,43 @@ def test_aux_count_input_words():
     @return Sin retorno
     """
 
-    obtained_count = aux_count_input_words('equ', 'isd')
+    obtained_count = aux_count_input_words('chi', 'por')
     expected_count = 3
-    first_count_success = obtained_count == expected_count
 
-    obtained_count = count_common_words('abs', 'aaq')
+    assert obtained_count == expected_count
+
+
+def test_aux_count_input_words_count_0():
+
+    """
+    Prueba de la función para obtener la cantidad de palabras que el primer
+    lenguaje aportó al otro
+    Entradas: No aplica
+    @return Sin retorno
+    """
+
+    obtained_count = count_common_words('nor', 'spa')
     expected_count = 0
-    second_count_success = obtained_count == expected_count
 
-    assert first_count_success and second_count_success
+    assert obtained_count == expected_count
 
 
 # ----------------------------------------------------------------------------
+
+
+def test_aux_count_words_received_count_5():
+
+    """
+    Prueba de la función para obtener la cantidad de palabras que cualquier
+    lenguaje aportó al segundo
+    Entradas: No aplica
+    @return Sin retorno
+    """
+
+    obtained_count = aux_count_words_received('por')
+    expected_count = 5
+
+    assert obtained_count == expected_count
 
 
 def test_aux_count_words_received():
@@ -126,21 +188,16 @@ def test_aux_count_words_received():
     @return Sin retorno
     """
 
-    obtained_count = aux_count_words_received('isd')
-    expected_count = 5
-    first_count_success = obtained_count == expected_count
-
-    obtained_count = count_common_words('abs', 'aaq')
+    obtained_count = aux_count_words_received('chi')
     expected_count = 0
-    second_count_success = obtained_count == expected_count
 
-    assert first_count_success and second_count_success
+    assert obtained_count == expected_count
 
 
 # ----------------------------------------------------------------------------
 
 
-def test_aux_input_percent():
+def test_aux_input_percent_60():
     """
     Prueba de la función para obtener el porcentaje que un lenguaje aportó
     al segundo
@@ -148,21 +205,29 @@ def test_aux_input_percent():
     @return Sin retorno
     """
 
-    obtained_percent = aux_input_percent('equ', 'isd')
+    obtained_percent = aux_input_percent('chi', 'por')
     expected_percent = 0.6
-    first_result_success = obtained_percent == expected_percent
 
-    obtained_percent = count_common_words('abs', 'aaq')
+    assert obtained_percent == expected_percent
+
+
+def test_aux_input_percent_0():
+    """
+    Prueba de la función para obtener el porcentaje que un lenguaje aportó
+    al segundo
+    Entradas: No aplica
+    @return Sin retorno
+    """
+
+    obtained_percent = aux_input_percent('ita', 'chi')
     expected_percent = 0
-    second_result_success = obtained_percent == expected_percent
-
-    assert first_result_success and second_result_success
+    assert obtained_percent == expected_percent
 
 
 # ----------------------------------------------------------------------------
 
 
-def test_get_all_lang_inputs():
+def test_get_all_lang_inputs_for_all():
     """
     Prueba de la función para obtener los porcentajes que cada lenguaje aportó
     a los otros
@@ -171,13 +236,50 @@ def test_get_all_lang_inputs():
     """
 
     obtained_results = get_all_lang_inputs()
-    expected_results = ['sss aporta a isd un 20.0%.',
-                        'equ aporta a isd un 60.0%.',
-                        'sas aporta a isd un 20.0%.',
-                        'aaq aporta a eng un 50.0%.',
-                        'abe aporta a eng un 50.0%.',
-                        'eng aporta a aaq un 100.0%.',
-                        'zsm aporta a abs un 100.0%.']
+    expected_results = ['chi aporta a nor un 100.0%.',
+                        'chi aporta a por un 60.0%.',
+                        'chi aporta a spa un 33.0%.',
+                        'ind aporta a ape un 33.0%.',
+                        'ind aporta a ind un 100.0%.',
+                        'ind aporta a por un 20.0%.',
+                        'lat aporta a ape un 33.0%.',
+                        'lat aporta a ita un 50.0%.',
+                        'nor aporta a ita un 50.0%.',
+                        'por aporta a ape un 33.0%.',
+                        'por aporta a lat un 100.0%.',
+                        'por aporta a spa un 33.0%.',
+                        'spa aporta a por un 20.0%.',
+                        'spa aporta a spa un 33.0%.']
+
+    assert obtained_results == expected_results
+
+
+def test_get_all_lang_inputs_for_one():
+    """
+    Prueba de la función para obtener los porcentajes que cada lenguaje aportó
+    a uno específico
+    Entradas: No aplica
+    @return Sin retorno
+    """
+
+    obtained_results = get_all_lang_inputs('por')
+    expected_results = ['chi aporta a por un 60.0%.',
+                        'ind aporta a por un 20.0%.',
+                        'spa aporta a por un 20.0%.']
+
+    assert obtained_results == expected_results
+
+
+def test_get_all_lang_inputs_no_input():
+    """
+    Prueba de la función para obtener los porcentajes que cada lenguaje aportó
+    a un lenguaje que no recibió aportes
+    Entradas: No aplica
+    @return Sin retorno
+    """
+
+    obtained_results = get_all_lang_inputs('chi')
+    expected_results = ['No hay aportes.']
 
     assert obtained_results == expected_results
 
@@ -185,14 +287,42 @@ def test_get_all_lang_inputs():
 # ----------------------------------------------------------------------------
 
 
-def test_get_max_input():
-
-    obtained_max = get_max_input('isd')
-    expected_max = 'Idioma: equ a: isd con 60.0%.'
-    first_result_success = obtained_max == expected_max
+def test_get_max_input_for_all():
+    """
+    Prueba de la función para obtener el máximo de los porcentajes que cada
+    lenguaje aportó a los otros
+    Entradas: No aplica
+    @return Sin retorno
+    """
 
     obtained_max = get_max_input()
-    expected_max = 'Idioma: zsm a: abs con 100.0%.'
-    second_result_success = obtained_max == expected_max
+    expected_max = 'Idioma: por a: lat con 100.0%.'
 
-    assert first_result_success and second_result_success
+    assert obtained_max == expected_max
+
+
+def test_get_max_input_for_one():
+    """
+    Prueba de la función para obtener el máximo de los porcentajes que cada
+    lenguaje aportó a uno específico
+    Entradas: No aplica
+    @return Sin retorno
+    """
+    obtained_max = get_max_input('por')
+    expected_max = 'Idioma: chi a: por con 60.0%.'
+
+    assert obtained_max == expected_max
+
+
+def test_get_max_input_for_one_empty():
+    """
+    Prueba de la función para obtener el máximo de los porcentajes que cada
+    lenguaje aportó a uno específico vacío
+    Entradas: No aplica
+    @return Sin retorno
+    """
+
+    obtained_max = get_max_input('chi')
+    expected_max = 'No hay aporte.'
+
+    assert obtained_max == expected_max
