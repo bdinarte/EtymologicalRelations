6562c606ef90343f6df413cc3c65521e0de6c5fc
Brandon <bdinarte@yahoo.com>
2018-06-17 22:23:46 -0600

    db thinner

--- tec/ic/ia/p2/util/database_thinner.py
+++ tec/ic/ia/p2/util/database_thinner.py
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------------------------
 
 import re
-
+from random import randint
 # -----------------------------------------------------------------------------
 
 valid_relations = ['etymological_origin_of', 'has_derived_form',
@@ -10,7 +10,7 @@ valid_relations = ['etymological_origin_of', 'has_derived_form',
 
 # -----------------------------------------------------------------------------
 
-target_langs = ['ben', 'tel', 'fao', 'bul']
+target_langs = []
 
 # -----------------------------------------------------------------------------
 
@@ -23,17 +23,19 @@ def generate_thinned_db(filename):
         u"([^:]+):\s*([^\s]+)\s*[^:]+:([^\s]+)\s*([^:]+):\s*(.+)",
         re.UNICODE
     )
-
+    lang_percent = 2
     with open(filename, 'r', encoding='utf8') as _input:
         with open('..\\files\\thinned.tsv', 'a', encoding='utf8') as _output:
             for i, line in enumerate(_input):
-                match = re.search(pattern, line)
+                if randint(0, 99) < lang_percent:
+                    match = re.search(pattern, line)
 
-                if match:
-                    if match.group(3) in valid_relations:
-                        if match.group(1) in target_langs or match.group(
-                                4) in target_langs:
-                            _output.write(line)
+                    if match:
+                        if match.group(3) in valid_relations:
+                            if match.group(1) not in target_langs or \
+                                    match.group(
+                                    4) not in target_langs:
+                                _output.write(line)
 
                 if int(i+1) % 50000 == 0:
                     print('Recortados: ' + str(i+1))
