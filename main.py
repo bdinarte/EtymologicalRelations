2f561e43f8d8b7f0b66d7868e1ddbf1f35881add
Julian Salinas <julian4001@outlook.com>
2018-06-17 22:31:24 -0600

    nuevo main.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 
 # -----------------------------------------------------------------------------
 
-from tec.ic.ia.p2.g03 import run
+import os
+import sys
+import logging
+from pyDatalog import pyDatalog, pyEngine
+
+parentdir = os.path.abspath(".")
+projectdir = os.path.join(parentdir, "tec", "ic", "ia", "p2")
+sys.path.insert(0, projectdir)
+
+from tec.ic.ia.p2.view.user_interface import *
+
+pyEngine.Logging = True
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
 # -----------------------------------------------------------------------------
+
+
+def run():
+    UserInterface().mainloop()
 
 if __name__ == '__main__':
     run()
